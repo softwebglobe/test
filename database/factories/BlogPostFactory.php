517c36efd3b1356bfb3e88096c196cc67b0a002f
SoftWeb Globe <softwebglobe@gmail.com>
2018-12-20 21:06:24 +0100

    creation of migration, factory, resources and model

---
+++ database/factories/BlogPostFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Model\BlogPost::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2018_12_20_193117_create_blog_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body');
+            $table->string('tags');
+            $table->float('reward')->default(0.0);
+            $table->integer('blog_view')->unsigned();
+            $table->integer('category_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_posts');
+    }
+}
